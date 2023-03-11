4a8d6fd39e308197db88c750243f47f8df824f35
jonhindsgaul <34034569+jonhindsgaul@users.noreply.github.com>
2023-03-11 10:03:44 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+import os
+import re
+from dotenv import load_dotenv
+import subprocess
+import sys
+from util.request_manager import request_manager
+
+rm = request_manager()
+last_request = ""
+
+while True:
+  print('Waiting for input...')
+  request = input()
+  if request == "exit":
+    break
+  elif request == "code":
+    print(last_request)
+    continue
+
+  rm.request = request
+  rm.request_type = 'code'
+  response = rm.query()
+  print("\nYour request is being processed...\n")
+  rm.exec_code(response)
+  last_request = response
+
+print("Finished...")
